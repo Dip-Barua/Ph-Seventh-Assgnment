c07edaa2f21d1f3f05aea2d7fad30754ac854992
Dip <65559847+Dip-Barua@users.noreply.github.com>
2024-10-25 17:41:58 +0600

    First Markup Commit with Banner section Submission

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Header from './components/header/Header';
+import Banner from './components/banner/Banner';
+export default function App() {
+  return (
+    <><Header></Header><Banner></Banner></>
+  )
+}
